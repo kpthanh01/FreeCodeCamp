833b0d04ef811f32fcbbb64ce092aaf6624956d5
Kevin Pham <kpthanh01@gmail.com>
2017-10-08 19:13:48 -0400

    Add files via upload

---
+++ Basic JavaScript Algorithms/chunky_monkey.js
@@ -0,0 +1,18 @@
+/*
+	Write a function that splits an array (first argument) into
+	groups the length of size (second argument) and returns them 
+	as a two-dimensional array
+*/
+
+
+
+function chunkArrayInGroups(arr, size){
+	var temp = [];
+	for (var i = 0; i < arr.length; i+=size) {
+		temp.push(arr.slice(i,i+size));
+	}
+	return temp
+}
+
+
+chunkArrayInGroups(["a", "b", "c", "d"], 2) // return [["a", "b"], ["c", "d"]]
