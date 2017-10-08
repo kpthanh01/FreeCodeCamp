6e025824c2b9008755248d1303444b1243b38d17
Kevin Pham <kpthanh01@gmail.com>
2017-10-08 05:16:30 -0400

    Add files via upload

---
+++ Basic JavaScript Algorithms/largest_number_array.js
@@ -0,0 +1,35 @@
+/*
+	The objective of this problem is to find the biggest number in each array
+	in this multi-dimensional array.
+*/
+
+
+function largestofFour(arr){
+	var largestFour = [];
+	for (var i = 0; i < arr.length; i++) {
+		var largest = arr[i][0];
+		for (var j = 1; i < arr[i].length; i++) {
+			if(largest < arr[i][j]){
+				largest = arr[i][j];
+			}
+		}
+		largestofFour.push(largest);
+	}
+	return largestofFour;
+}
+
+/*
+This is more advance way to approach the problem by using .map(), .reduce(), and 
+ternary condition.
+
+	function largestOfFour(arr) {
+	  return arr.map(function(group){
+	    return group.reduce(function(prev, current) {
+	      return (current > prev) ? current : prev;
+	    });
+	  });
+	}
+*/
+
+largestofFour([[4, 5, 1, 3], [13, 27, 18, 26], [32, 35, 37, 39], [1000, 1001, 857, 1]])
+// return [5,27,39,1001]
